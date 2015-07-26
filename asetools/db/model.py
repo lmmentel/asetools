be924974caebc14aaf11a55dbc2ebbc6d90b7297
Lukasz Mentel <lmmentel@gmail.com>
2015-07-26 14:38:10 +0200

    added a draft of the db code

---
+++ asetools/db/model.py
@@ -0,0 +1,91 @@
+
+''' a database for storing the ase atoms abjects'''
+
+from sqlalchemy import Column, LargeBinary, Integer, String, Float, PickleType, create_engine, ForeignKey, DateTime
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
+import os
+from operator import attrgetter
+import datetime
+
+Base = declarative_base()
+
+class Species(Base):
+
+    __tablename__ = 'species'
+
+    id = Column(Integer, primary_key=True)
+    z = Column(Integer)
+    n = Column(Integer)
+
+class Label(Base):
+
+    __tablename__ = 'labels'
+
+    id = Column(Integer, primary_key=True)
+    system_id = Column(Integer, ForeignKey('systems.id'))
+    label = Column(String)
+
+    def __init__(self, label):
+        self.label = label
+
+class Kwarg(Base):
+
+    __tablename__ = 'kwargs'
+
+    id = Column(Integer, primary_key=True)
+    system_id = Column(Integer, ForeignKey('systems.id'))
+    key = Column(String)
+    value = Column(String)
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+class System(Base):
+
+    __tablename__ = 'systems'
+
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+    username = Column(String)
+
+    atoms = Column(PickleType)
+
+    energy = Column(Float)
+    free_energy = Column(Float)
+    forces = Column(PickleType)
+    stress = Column(PickleType)
+    magmom = Column(PickleType)
+
+    _labels = relationship(Label)
+    _kwargs = relationship(Kwarg)
+
+
+    @hybrid_property
+    def labels(self):
+        return [l.label for l in self._labels]
+
+    @hybrid_property
+    def kwargs(self):
+        return {kw.key:kw.value for kw in self._kwargs}
+
+def get_session(dbpath):
+    '''Return the database session connection.'''
+
+    if os.path.exists(dbpath):
+        engine = create_engine("sqlite:///{path:s}".format(path=dbpath), echo=False)
+    else:
+        engine = create_engine("sqlite:///{path:s}".format(path=dbpath), echo=False)
+        Base.metadata.create_all(engine)
+    db_session =  sessionmaker(bind=engine, autoflush=False, autocommit=False)
+    return db_session()
+
+def get_engine(dbpath):
+    '''Return the db engine'''
+
+    engine = create_engine("sqlite:///{path:s}".format(path=dbpath), echo=False)
+    return engine
+
