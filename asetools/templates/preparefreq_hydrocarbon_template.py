e85cb523b242edeed1da9237bf599932c50fcce5
Rasmus Brogaard <r.y.brogaard@smn.uio.no>
2015-11-18 10:58:15 +0100

    removed obsolete templates

--- asetools/templates/preparefreq_hydrocarbon_template.py
+++
@@ -1,9 +0,0 @@
-from ase.all import *
-from gpaw import *
-
-atoms = read('$input')
-
-energy = atoms.get_potential_energy()
-#atoms.get_calculator().write('$out.gpw',mode='all')
-density = atoms.calc.get_all_electron_density() * Bohr**3
-write('$out.cube',atoms,data=density)
