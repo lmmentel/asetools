3699fcf72d158d5d263ccebd6abdb60ac5c110a3
Lukasz Mentel <lmmentel@gmail.com>
2015-11-26 22:51:44 +0100

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -67,5 +67,5 @@ setup(
     long_description = open("README.md", "r").read(),
     name = "asetools",
     url = "www.bitbucket.org/lukaszmentel/asetools",
-    version = "0.1.2",
+    version = "0.1.3",
 )
