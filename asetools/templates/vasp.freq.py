e0bff6f65adc82a668da13a3a0c24af4ec02d76c
Lukasz Mentel <lmmentel@gmail.com>
2017-01-04 01:26:46 +0100

    updated the NSW and xc values

--- asetools/templates/vasp.freq.py
+++ asetools/templates/vasp.freq.py
@@ -7,12 +7,13 @@ atoms = ase.io.read('%initial')
 
 calc = Vasp(
         prec='Accurate',
+        xc='PBE',
         gga='PE',
         lreal=False,
         ediff=1.0e-8,
         encut=600.0,
         nelmin=5,
-        nsw=10000,
+        nsw=1,
         nelm=100,
         ediffg=-0.001,
         ismear=0,
