d55073b44b59255ca2d4fa6101c5dd2bebcd2fe6
Lukasz Mentel <lmmentel@gmail.com>
2017-01-04 11:55:43 +0100

    added and updated the template for hessian + thermo calculation

--- asetools/templates/vasp.freq.py
+++ asetools/templates/vasp.freq.py
@@ -1,36 +1,33 @@
 
-from ase import Atoms, Atom
 import ase.io
 from ase.calculators.vasp import Vasp
 
 atoms = ase.io.read('%initial')
 
-calc = Vasp(
-        prec='Accurate',
-        xc='PBE',
-        gga='PE',
-        lreal=False,
-        ediff=1.0e-8,
-        encut=600.0,
-        nelmin=5,
-        nsw=1,
-        nelm=100,
-        ediffg=-0.001,
-        ismear=0,
-        ibrion=5,
-        potim=0.02,
-        nfree=2,
-        isym=0,
-        lvdw=True,
-        lcharg=False,
-        lwave=False,
-        istart=0,
-        npar=2,
-        ialgo=48,
-        lplane=True,
-        ispin=1,
-    )
+calc = Vasp(prec='Accurate',
+            xc='PBE',
+            gga='PE',
+            lreal=False,
+            ediff=1.0e-8,
+            encut=600.0,
+            nelmin=5,
+            nsw=1,
+            nelm=100,
+            ediffg=-0.001,
+            ismear=0,
+            ibrion=5,
+            potim=0.02,
+            nfree=2,
+            isym=0,
+            lvdw=True,
+            lcharg=False,
+            lwave=False,
+            istart=0,
+            npar=2,
+            ialgo=48,
+            lplane=True,
+            ispin=1,
+            )
 
 atoms.set_calculator(calc)
 calc.calculate(atoms)
-
